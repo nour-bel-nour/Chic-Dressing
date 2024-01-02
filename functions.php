bf4fb835dc270c2502e1ffac061fed8d9707ce90
Nourdine Belkhadir <140583027+nour-bel-nour@users.noreply.github.com>
2024-01-02 15:41:40 +0100

    final

--- functions.php
+++ functions.php
@@ -7,7 +7,17 @@ function chicdressing_enqueue_styles() {
 
 add_filter( 'big_image_size_threshold', '__return_false' );
 
+ 
+add_action( 'wp_enqueue_scripts', 'deregister_fonts', 20 ); //Asch 282
+function deregister_fonts() {
+    wp_dequeue_style( 'ashe-playfair-font' );
+    wp_dequeue_style( 'ashe-opensans-font' );
+    wp_dequeue_style( 'ashe-kalam-font' );
+    wp_dequeue_style( 'ashe-rokkitt-font' );
+}
 
+
+ 
 
 
  
